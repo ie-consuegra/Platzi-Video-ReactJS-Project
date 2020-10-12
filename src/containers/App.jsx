579f3c3b83bd6fb41ee5269e4b34dcee1e6920e5
Ivan Consuegra <ie.consuegra@gmail.com>
2020-10-12 10:03:03 -0500

    UseEffect and UseState using a local server that doesn't work

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -9,26 +9,38 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
+
+  const [videos, setVideos] = useState({ 'myList': [], 'trends': [], 'originals': [] }); // useState recibe diferentes tipos de datos booleanos, arreglos, strings
+
+  // useEffect recibe dos parámetros el primero es una función y el segundo es una propiedad que escucha cambios en propiedades para volver a ejecutarse, si no se pasa ese parámetro se ejecuta indefinidamente
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState/')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Search />
-
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        videos.myList?.length > 0 &&
+        (
+          <Categories title="Mi lista">
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+        )
+      }
 
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {
+            videos.trends?.forEach((item) => {
+              <CarouselItem key={item.id} {...item} />;
+            })
+          }
         </Carousel>
       </Categories>
 
