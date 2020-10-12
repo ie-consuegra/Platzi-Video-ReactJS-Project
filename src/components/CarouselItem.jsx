bf3be11bf7262e751fbc88b29b0192ea0d4ff952
Ivan Consuegra <ie.consuegra@gmail.com>
2020-10-12 13:46:28 -0500

    Using TVMaze API since the local server didn't work, iteration to generate several carousel items did not work

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,18 +3,18 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => {
+const CarouselItem = ({ image, name, premiered, rating }) => {
   return (
     <div className="carousel-item">
-      <img className="carousel-item__img" src={cover} alt={title} />
+      <img className="carousel-item__img" src={image} alt={name} />
       <div className="carousel-item__details">
         <div>
           <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
           <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" />
         </div>
-        <p className="carousel-item__details--title">{title}</p>
+        <p className="carousel-item__details--title">{name}</p>
         <p className="carousel-item__details--subtitle">
-          {`${year} ${contentRating} ${duration}`}
+          {`${premiered} avg. rating: ${rating}`}
         </p>
       </div>
     </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -10,48 +10,94 @@ import '../assets/styles/App.scss';
 
 const App = () => {
 
-  const [videos, setVideos] = useState({ 'myList': [], 'trends': [], 'originals': [] }); // useState recibe diferentes tipos de datos booleanos, arreglos, strings
+  async function getTvShow(id) {
+    const url = `http://api.tvmaze.com/shows/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  // Generate an array with a specified quantity of random numbers (ids)
+  function getArrOfIds(maxNumOfItems = 3) {
+    const arrOfIds = [];
+    const maxIdNumber = 2000; // TVMaze shows ids can be even higher,  but I'll just work with ids from 1 to 2000 max
+    while (arrOfIds.length <= maxNumOfItems) {
+      const randomId = Math.floor(Math.random() * maxIdNumber);
+      // Avoid any random id appears more than once in arrOfIds
+      if (arrOfIds.indexOf(randomId) === -1) {
+        arrOfIds.push(randomId);
+      }
+    }
+    return arrOfIds;
+  }
+
+  async function getTvShows() {
+    const numOfItems = 2;
+    const arrOfIds = getArrOfIds(numOfItems);
+
+    const tvShowPromises = arrOfIds.map((id) => getTvShow(id));
+
+    const tvShows = await Promise.all(tvShowPromises);
+    return tvShows;
+  }
+
+  const [videos, setVideos] = useState([]); // useState recibe diferentes tipos de datos booleanos, arreglos, strings
 
   // useEffect recibe dos parámetros el primero es una función y el segundo es una propiedad que escucha cambios en propiedades para volver a ejecutarse, si no se pasa ese parámetro se ejecuta indefinidamente
   useEffect(() => {
-    fetch('http://localhost:3000/initialState/')
-      .then((response) => response.json())
+    const tvShows = getTvShows();
+    tvShows
       .then((data) => setVideos(data));
   }, []);
+
+  console.log(videos);
 
   return (
     <div className="App">
       <Header />
       <Search />
-      {
-        videos.myList?.length > 0 &&
-        (
-          <Categories title="Mi lista">
-            <Carousel>
-              <CarouselItem />
-            </Carousel>
-          </Categories>
-        )
-      }
 
-      <Categories title="Tendencias">
-        <Carousel>
-          {
-            videos.trends?.forEach((item) => {
-              <CarouselItem key={item.id} {...item} />;
-            })
-          }
-        </Carousel>
-      </Categories>
+      { videos.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
 
-      <Categories title="Originales de Platzi Video">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+            <CarouselItem image={videos[0]?.image?.original} name={videos[0]?.name} rating={videos[0]?.rating?.average} premiered={videos[0]?.premiered} />
+
+            <CarouselItem image={videos[1]?.image?.original} name={videos[1]?.name} rating={videos[1]?.rating?.average} premiered={videos[1]?.premiered} />
+
+            <CarouselItem image={videos[2]?.image?.original} name={videos[2]?.name} rating={videos[2]?.rating?.average} premiered={videos[2]?.premiered} />
+
+          </Carousel>
+        </Categories>
+      )}
+
+      { videos.length > 0 && (
+        <Categories title="Tendencias">
+          <Carousel>
+
+            <CarouselItem image={videos[0]?.image?.original} name={videos[0]?.name} rating={videos[0]?.rating?.average} premiered={videos[0]?.premiered} />
+
+            <CarouselItem image={videos[1]?.image?.original} name={videos[1]?.name} rating={videos[1]?.rating?.average} premiered={videos[1]?.premiered} />
+
+            <CarouselItem image={videos[2]?.image?.original} name={videos[2]?.name} rating={videos[2]?.rating?.average} premiered={videos[2]?.premiered} />
+
+          </Carousel>
+        </Categories>
+      )}
+
+      { videos.length > 0 && (
+        <Categories title="Populares ahora">
+          <Carousel>
+
+            <CarouselItem image={videos[0]?.image?.original} name={videos[0]?.name} rating={videos[0]?.rating?.average} premiered={videos[0]?.premiered} />
+
+            <CarouselItem image={videos[1]?.image?.original} name={videos[1]?.name} rating={videos[1]?.rating?.average} premiered={videos[1]?.premiered} />
+
+            <CarouselItem image={videos[2]?.image?.original} name={videos[2]?.name} rating={videos[2]?.rating?.average} premiered={videos[2]?.premiered} />
+
+          </Carousel>
+        </Categories>
+      )}
 
       <Footer />
     </div>
